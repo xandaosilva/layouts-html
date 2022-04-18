c91e919c2d0a516f1b3b6f0d9074b1111494da1c
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-04-18 12:14:42 -0300

    Arquivo script.js

---
+++ ITSolutions/assets/js/script.js
@@ -0,0 +1,14 @@
+$("#navbar a, .btn").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        const hash = this.hash;
+
+        $("html, body").animate({
+            scrollTop: $(hash).offset().top - 100
+        },800);
+    }
+});
+
+window.addEventListener("scroll", function () {
+    (window.scrollY > 150) ? document.querySelector("#navbar").style.opacity = 0.9 : document.querySelector("#navbar").style.opacity = 1;
+});
