cf3f9d902dbc65c2542eee6ee3e473b8f5f8a0c3
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-04-19 17:04:18 -0300

    Verificando vitória no Jogo da velha

--- JogoDaVelha/assets/js/script.js
+++ JogoDaVelha/assets/js/script.js
@@ -1,7 +1,14 @@
 const cellElements = document.querySelectorAll("[data-cell]");
 const board = document.querySelector("[data-board]");
+const winningMessageTextElement = document.querySelector("[data-winning-message-text]");
+const winningMessage = document.querySelector("[data-winning-message]");
 
 let isCircleTurn;
+
+const winningCombinations = [
+    [0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],
+    [1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6],
+];
 
 const startGame = () => {
     for (const cell of cellElements) {
@@ -10,6 +17,21 @@ const startGame = () => {
 
     isCircleTurn = false;
     board.classList.add("x");
+}
+
+const endGame = (isDraw) => {
+    if(isDraw) winningMessageTextElement.innerText = "Empate";
+    else winningMessageTextElement.innerText = isCircleTurn ? "Círculo venceu" : "X venceu";
+
+    winningMessage.classList.add("show-winning-message");
+}
+
+const checkForWin = (currentPlayer) => {
+    return winningCombinations.some(combination =>{
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentPlayer);
+        });
+    });
 }
 
 const placeMark = (cell, classToAdd) => {
@@ -31,6 +53,12 @@ const handleClick = (e) => {
     const cell = e.target;
     const classToAdd = isCircleTurn ? "circle" : "x";
     placeMark(cell,classToAdd);
+
+    /* Verificar por vitória */
+    const isWin = checkForWin(classToAdd);
+
+    if(isWin)
+        endGame(false);
 
     /* Alternando turnos */
     swapTurns();
