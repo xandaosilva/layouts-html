17988dd599233a8a4397d2794a1378ca9af83095
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-04-19 16:39:01 -0300

    Alternando turnos no Jogo da velha

--- JogoDaVelha/assets/js/script.js
+++ JogoDaVelha/assets/js/script.js
@@ -1,17 +1,39 @@
 const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.querySelector("[data-board]");
 
 let isCircleTurn;
+
+const startGame = () => {
+    for (const cell of cellElements) {
+        cell.addEventListener("click", handleClick, { once: true });
+    }
+
+    isCircleTurn = false;
+    board.classList.add("x");
+}
 
 const placeMark = (cell, classToAdd) => {
     cell.classList.add(classToAdd);
 }
 
-const handleClick = (e) => {
-    const cell = e.target;
-    const classToAdd = isCircleTurn ? "circle" : "x";
-    placeMark(cell,classToAdd)
+const swapTurns = () => {
+    isCircleTurn = !isCircleTurn;
+
+    board.classList.remove("circle");
+    board.classList.remove("x");
+
+    if(isCircleTurn) board.classList.add("circle");
+    else board.classList.add("x");
 }
 
-for (const cell of cellElements) {
-    cell.addEventListener("click", handleClick, { once: true });
+const handleClick = (e) => {
+    /* Adicionando marca no jogo */
+    const cell = e.target;
+    const classToAdd = isCircleTurn ? "circle" : "x";
+    placeMark(cell,classToAdd);
+
+    /* Alternando turnos */
+    swapTurns();
 }
+
+startGame();
