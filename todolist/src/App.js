b7a22204538b659f77f8fbf377fcf2d8746ec071
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-04-22 10:34:38 -0300

    Criando componente de exibição de tarefas

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,8 +1,32 @@
+import React, { useState } from "react";
+import Tasks from "./components/Tasks";
 import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar desenvolvimento web",
+      completed: false
+    },
+    {
+      id: 2,
+      title: "Almoçar",
+      completed: false
+    },
+    {
+      id: 3,
+      title: "Dormir",
+      completed: false
+    }
+  ]);
+
   return (
-    <div className="container"></div>
+    <>
+      <div className="container">
+        <Tasks tasks={tasks}/>
+      </div>
+    </>
   );
 }
 
