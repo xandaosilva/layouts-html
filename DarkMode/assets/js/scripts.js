17b4eb1776ceb837c5c1c1c1eb1d2163d58f743c
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-07-12 23:43:18 -0300

    Finalizando script js do projeto Dark mode

--- DarkMode/assets/js/scripts.js
+++ DarkMode/assets/js/scripts.js
@@ -1,5 +1,23 @@
 const changeThemeBtn = document.querySelector("#change-theme");
 
-changeThemeBtn.addEventListener("change", function(){
+function toggleDarkMode(){
     document.body.classList.toggle("dark");
+}
+
+function loadTheme(){
+    const darkMode = localStorage.getItem("dark");
+
+    if(darkMode)
+        toggleDarkMode();
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function(){
+    toggleDarkMode();
+
+    localStorage.removeItem("dark");
+
+    if(document.body.classList.contains("dark"))
+        localStorage.setItem("dark", 1);
 });
